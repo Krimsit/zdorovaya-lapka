38a38abc803a928b9fbae6f0b01dbf0720f58ed2
Andrey Subbotin <andrey.subbotin@altenar.com>
2025-06-03 15:35:44 +0300

    Fix HTTPS

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(krimsit.ru www.krimsit.ru)
+domains=(krimsit.ru)
 rsa_key_size=4096
 data_path="./nginx/ssl"
 email="krimsitdjenkis@gmail.com"
